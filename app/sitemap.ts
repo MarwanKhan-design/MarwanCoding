3cacfaf0ad6a02f923f0e9a3da573ebae82d4e3d
Marwan Khan <58033457+MarwanKhan-design@users.noreply.github.com>
2026-09-16 21:57:34 +0500

    added robots.ts and sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://marwankhan.com",
+      lastModified: new Date(),
+    },
+  ];
+}
